615bc8d6b3d75c7dfd3a9b80125c6db48b4ff632
Shweta kale <shwetakale144@gmail.com>
2024-05-09 13:29:18 +0530

    trying to set blob

---
+++ client/functions/test.ts
@@ -0,0 +1,20 @@
+import { getStore } from "@netlify/blobs";
+import type { Context } from "@netlify/functions";
+
+export default async (req: Request, context: Context) => {
+  const construction = getStore("construction");
+  await construction.set("nails", "For general carpentry");
+
+  const beauty = getStore("beauty");
+  await beauty.set("nails", "For formal events", {
+    metadata: { material: "acrylic", sale: true },
+  });
+  return new Response("Nail blobs set for Construction and Beauty stores");
+
+  // const resp = await beauty.getWithMetadata("nails");
+  // if(resp){
+  //   const { data, etag, metadata } = resp;
+  //   return Response.json({ data, etag, metadata });
+  // }
+  // return Response.json('failed to load')
+};
